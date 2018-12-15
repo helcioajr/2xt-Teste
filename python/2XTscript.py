2eb86c2f604edcb438bbb2f84b6ceffbc64310de
Helcio Alves <helcioajr@gmail.com>
2018-12-15 20:22:54 -0200

    teste

--- python/2XTscript.py
+++ python/2XTscript.py
@@ -1,6 +1,8 @@
 import requests
 import json
 
+apiKey = "phnvlDweeuDLvjSqCqre9mOzvKKdzMmw"
+
 r = requests.get('http://stub.2xt.com.br/air/airports/phnvlDweeuDLvjSqCqre9mOzvKKdzMmw', auth=('helcio', 'sejvlD'))
 
 airport_list = json.loads(r.text)
